7e87ab2fb1db914f4bfca7a81b0a127cf270d15e
Georgios Theodoridis <Georgios.Theodoridis@students.ittralee.ie>
2019-09-19 11:47:28 +0100

    First

---
+++ src/Problem1.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+
+public class Problem1 {
+
+    final float exchangeRate = 1.13f;
+
+
+    public static void main(String args[]) {
+
+
+        Scanner input = new Scanner(System.in);
+        float amount = input.nextInt();
+
+        while ( int i = 1;i< 5;i++){
+            
+            System.out.print("Enter amount: ");
+
+
+        }
+
+        public float rate ( float a){
+
+            float result = a * exchangeRate;
+            return result;
+
+
+        }
+
+
+    }
+}
